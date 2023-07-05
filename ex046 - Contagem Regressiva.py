7c68201a44d62e08e64b7a9f458df565a61425ea
Mark-Sprone <marcos.sprone@hotmail.com>
2023-07-05 01:42:15 -0300

    .

---
+++ ex046 - Contagem Regressiva.py
@@ -0,0 +1,11 @@
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+import time
+
+for fogos in range(10, 0, -1):
+    time.sleep(1)
+    print(fogos)
+print('FELIZ ANO NOVO!')
